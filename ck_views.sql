2b09231f4ec906c31477231d813280e4c8cef8c0
Dbakevlar <dbakevlar@gmail.com>
2018-10-25 12:41:14 -0700

    Updated with Step II of logical deployment
    SQL Scripts to support logical objects inside staging and datawarehouse and updates to support them into deploy script.
    Added dynamic firewall rule for Azure cloud shell.

---
+++ ck_views.sql
@@ -0,0 +1,21 @@
+/* Check Script for Views Post Data Load */
+SELECT * FROM AcademicLevel
+GO
+SELECT * FROM AcademicProgram
+GO
+SELECT * FROM Admission
+GO
+SELECT * FROM AdmitType
+GO
+SELECT * FROM ApplicationSource
+GO
+SELECT * FROM Class
+GO
+SELECT * FROM Details
+GO
+SELECT * FROM ResidencyStatus
+GO
+SELECT * FROM Student
+GO
+SELECT * FROM Summary
+GO
--- edu_deploy.sh
+++ edu_deploy.sh
@@ -6,23 +6,24 @@ IFS=$'\n\t'
 # edu_deploy.sh
 # Author: Kellyn Gorman
 # Deploys EDU Solution via Azure CLI to Azure
+# Initial Script- 10/09/2018
 #---------------------------------------------------------------
 # -e: immediately exit if anything is missing
 # -o: prevents masked errors
 # IFS: deters from bugs, looping arrays or arguments (e.g. $@)
 #---------------------------------------------------------------
 
-usage() { echo "Usage: $0 -i <subscriptionID> -g <groupname> -u <adminlogin> -p <password> -s <servername> -l <zone>" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -i <subscriptionID> -g <groupname> -p <password> -pp <proxypassword> -s <servername> -l <zone>" 1>&2; exit 1; }
 
 declare subscriptionID=""
 declare groupname=""
-declare adminlogin=""
 declare password=""
+declare proxypassword=""
 declare servername=""
 declare zone=""
 
 # Initialize parameters specified from command line
-while getopts ":i:g:u:p:s:l:" arg; do
+while getopts ":i:g:p:pp:s:l:" arg; do
         case "${arg}" in
                 i)
                         subscriptionID=${OPTARG}
@@ -30,11 +31,11 @@ while getopts ":i:g:u:p:s:l:" arg; do
                 g)
                         groupname=${OPTARG}
                         ;;
-                u)
-                        adminlogin=${OPTARG}
-                        ;;
                 p)
                         password=${OPTARG}
+                        ;;
+                pp)
+                        proxypassword=${OPTARG}
                         ;;
                 s)
                         servername=${OPTARG}
@@ -42,7 +43,7 @@ while getopts ":i:g:u:p:s:l:" arg; do
                 l)
                         zone=${OPTARG}
                         ;;
-                esac
+                 esac
 done
 shift $((OPTIND-1))
 
@@ -58,7 +59,7 @@ templateFile2="template2.json"
 
 if [ ! -f "$templateFile2" ]; then
         echo "$templateFile2 not found"
-                exit 1
+        exit 1
 fi
 
 #parameter files to be used
@@ -78,6 +79,7 @@ fi
 
 #Prompt for parameters is some required parameters are missing
 #login to azure using your credentials
+
 echo "Here is your Subscription ID Information"
 az account show | grep id
 
@@ -94,72 +96,50 @@ if [[ -z "$groupname" ]]; then
         [[ "${groupname:?}" ]]
 fi
 
-if [[ -z "$adminlogin" ]]; then
-        echo "What name would you like for your Database admin login?  Example sqladmin "
-        echo "Enter the login name "
-        read adminlogin
-        [[ "${adminlogin:?}" ]]
+if [[ -z "$password" ]]; then
+        echo "Your database login will be sqladmin and you'll need a password for this login?"
+        echo "Password must meet requirements for sql server, including capilatization, special characters.  Example: SQLAdm1nt3st1ng!"
+        echo "Enter the login password "
+        read password
+        [[ "${password:?}" ]]
 fi
 
-if [[ -z "$password" ]]; then
-        echo "Password must meet requirements for sql server, including capilatization, special characters.  Example: SQLAdm1nt3st1ng!"
-        echo "Enter a password for your database admin login:"
-        read password
+if [[ -z "$proxypassword" ]]; then
+        echo "This password is for the proxy user that will work wtih the objects in the databases.  Use only letters and numbers, as special characters can create issues.  Example: SQLAdm1nt3st1ng"
+        echo "Enter a password for your database proxy login:"
+        read proxypassword
+        [[ "${proxypassword:?}" ]]
 fi
 
 if [[ -z "$servername" ]]; then
         echo "What would you like for the name of the server that will house your sql databases? Example hiededusql1 "
         echo "Enter the server name:"
         read servername
+        [[ "${servername:?}" ]]
 fi
 
 if [[ -z "$zone" ]]; then
         echo "Finally, what will be the Azure location zone to create everything in? Example eastus or centralus "
         echo "Enter the location name:"
         read zone
+        [[ "${zone:?}" ]]
 fi
 
-# The ip address range that you want to allow to access your DB. This can be updated later on in the portal, so defaults can remain.
-export startip=67.100.0.0
-export endip=72.82.0.0
+# The ip address range that you want to allow to access your DB.
+# This rule is used by Visual Studio and SSMS to access and load data.  The firewall is dynamically offered for the workstation, but not for the Azure Cloud Shell.
+# Added dynamic pull for IP Address for firewall rule 10/23/2018, KGorman
+# Added a logfile and a static sqladmin for the database login.
+
+echo "getting IP Address for Azure Cloud Shell for firewall rule"
+export myip=$(curl http://ifconfig.me)
+export startip=$myip
+export endip=$myip
+export logfile="/home/kellyn/EDU/ck_views.log"
+export adminlogin=sqladmin
 
 #---------------------------------------------------------------
 # Customers should only update the variables in the top of the script, nothing below this line.
-#---------------------------------------------------------------
-
-# Set default subscription ID if not already set by customer.
-az account set --subscription $subscriptionID
-
-# Create a resource group
-az group create \
-        --name $groupname \
-        --location $zone
-
-# Create a logical server in the resource group
-az sql server create \
-        --name $servername \
-        --resource-group $groupname \
-        --location $zone  \
-        --admin-user $adminlogin \
-        --admin-password $password
-
-
-# Configure a firewall rule for the server
-az sql server firewall-rule create \
-        --resource-group $groupname \
-        --server $servername \
-                -n AllowYourIp \
-        --start-ip-address $startip \
-        --end-ip-address $endip
-
-az configure --defaults sql-server=$servername
-# Create a database for the staging database
-az sql db create \
-        --resource-group $groupname \
-        --name HiEd_Staging \
-        --service-objective S0 \
-        --capacity 10 \
-        --zone-redundant false
+#--
 
 # Create a database  for the data warehouse
 az sql db create \
@@ -170,12 +150,14 @@ az sql db create \
         --zone-redundant false
 
 
-#Deploy Azure Analysis Server and ADF
-#Start deployment
+# Deploy Azure Analysis Server and ADF
+# Added 10/16/2018
+# Start deployment
 (
         set -x
         az group deployment create --resource-group "$groupname" --template-file "$templateFile1" --parameters "@${parametersFile1}"
 )
+
 if [ $?  == 0 ];
  then
         echo "Azure Data Factory has been successfully deployed"
@@ -193,4 +175,26 @@ if [ $?  == 0 ];
         echo "Azure Analysis Server has been successfully deployed"
 fi
 
-echo "All resources should have been deployed successfully.  Check Resource Group in Azure Portal."
+echo "This Completes Part I, the physical resources deployment, Part II will now begin."
+
+# Populate Database Objects
+# Added 10/23/18, KGorman
+# Added proxy login to support scripts and added check log for post deploy. 10/25/2018
+# DataWarehouse
+echo "Part II logs into the SQL Server and deploys the logical objects and support structures."
+sqlcmd -U $adminlogin -S"${servername}.database.windows.net" -P "$password" -d master -Q "CREATE LOGIN ProxyUser WITH PASSWORD = '${proxypassword}'; "
+
+sqlcmd -U $adminlogin -S "${servername}.database.windows.net" -P "$password" -d HiEd_DW -i "edu_hied_DW.sql"
+
+# DataStaging
+sqlcmd -U $adminlogin -S "${servername}.database.windows.net" -P "$password" -d HiEd_Staging -i "edu_hied_staging.sql"
+
+# Check for data and push to output file
+sqlcmd -U $adminlogin -S "${servername}.database.windows.net" -P "$password" -d HiEd_Staging -i "ck_views.sql" > $logfile
+
+echo "This is your servername:" $servername >> edu_deploy.txt
+echo "This is your Admin User and Password:" $adminlogin |"/"| $password >> edu_deploy.txt
+echo "This is your Azure location zone:" $zone >> edu_deploy.txt
+echo "This is the subscription deployed to and the Firewall IP:" ${subscriptionID} |", "|${myip} >> edu_deploy.txt
+
+echo "Part II is now complete."
