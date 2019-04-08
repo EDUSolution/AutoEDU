93a5bc24f2e617f68634b59bc6eabb8aa7687142
Dbakevlar <dbakevlar@gmail.com>
2019-04-08 13:21:17 -0400

    Add files via upload
    Updated after testing logfile and json dynamic population

--- edu_deploy.sh
+++ edu_deploy.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
+# Set Job Run name here for logging:
+export jobrun=edu_deploy
 
 #---------------------------------------------------------------
 # edu_deploy.sh
@@ -13,18 +15,20 @@ IFS=$'\n\t'
 # IFS: deters from bugs, looping arrays or arguments (e.g. $@)
 #---------------------------------------------------------------
 
-usage() { echo "Usage: $0 -i <subscriptionID> -g <groupname> -p <password> -pp <proxypassword> -s <servername> -l <zone> -d <data>" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -i <subscriptionID> -g <groupname> -p <password> -pp <proxypassword> -s <servername> -ad <adfname> -as <aasname> -l <zone> -d <data>" 1>&2; exit 1; }
 
 declare subscriptionID=""
 declare groupname=""
 declare password=""
 declare proxypassword=""
 declare servername=""
+declare adfname=""
+declare aasname=""
 declare zone=""
 declare data=""
 
 # Initialize parameters specified from command line
-while getopts ":i:g:p:pp:s:l:d:" arg; do
+while getopts ":i:g:p:pp:s:ad:as:l:d:" arg; do
 	case "${arg}" in
 		i)
 			subscriptionID=${OPTARG}
@@ -40,6 +44,12 @@ while getopts ":i:g:p:pp:s:l:d:" arg; do
 			;;
 		s)
 			servername=${OPTARG}
+			;;
+		ad)
+			adfname=${OPTARG}
+			;;
+		as)
+			aasname=${OPTARG}
 			;;
 		l)
 			zone=${OPTARG}
@@ -122,6 +132,21 @@ if [[ -z "$servername" ]]; then
 	[[ "${servername:?}" ]]
 fi
 
+if [[ -z "$adfname" ]]; then
+	echo "What would you like for the name of the Azure Data Factory.  This MUST be GLOBALLY UNIQUE, all small letters.  Example adfxxxx1 "
+	
+        echo "Enter the Azure Data Factory name:"
+	read adfname
+	[[ "${adfname:?}" ]]
+fi
+
+if [[ -z "$aasname" ]]; then
+	echo "What would you like for the name of the Azure Analysis Service?  This MUST ALSO be GLOBALLY UNIQUE, all small leteters.  Example xxxxxaas1 "
+	echo "Enter the Azure Analysis Services name:"
+	read aasname
+	[[ "${aasname:?}" ]]
+fi
+
 if [[ -z "$zone" ]]; then
 	echo "What will be the Azure location zone to create everything in? Example eastus or centralus "
 	echo "Enter the location name:"
@@ -130,7 +155,7 @@ if [[ -z "$zone" ]]; then
 fi
 
 if [[ -z "$data" ]]; then
-	echo "Finally, will you be using the example data load vs. using your own data?  If using the example data, type in <Y> for yes."
+	echo "Finally, will you be using the example data load vs. using your own data?  If using the example data, answer 1 for yes or 2 for no."
 	read data
 	[[ "${data:?}" ]]
 fi
@@ -144,14 +169,64 @@ echo "getting IP Address for Azure Cloud Shell for firewall rule"
 export myip=$(curl http://ifconfig.me)
 export startip=$myip
 export endip=$myip
+export logfile=./$jobrun.txt
+rm $logfile
 export adminlogin=sqladmin
+export schema='$schema'
+
+
+# Update the JSON files with the correct resource names and zones.
+
+cat >./$parametersFile1 <<EOF
+{
+    "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "name": {
+            "value": "$adfname"
+        },
+        "location": {
+            "value": "$zone"
+        },
+        "apiVersion": {
+            "value": "2018-06-01"
+        }
+    }
+}
+EOF
+
+cat >./$parametersFile2 <<EOF
+{
+    "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "name": {
+            "value": "$aasname"
+        },
+        "location": {
+            "value": "$zone"
+        },
+        "sku": {
+            "value": "B1"
+        },
+        "backupBlobContainerUri": {
+            "value": ""
+        },
+        "managedMode": {
+            "value": 1
+        }
+    }
+}
+EOF
 
 #---------------------------------------------------------------
 # Customers should only update the variables in the top of the script, nothing below this line.
 #---------------------------------------------------------------
 
 # Unzip data load files
+rm -rf ./hied_data
 unzip hied_data.zip
+mv ./hied_data/* ../.
 
 # Set default subscription ID if not already set by customer.
 # Created on 10/14/2018
@@ -200,7 +275,7 @@ az sql db create \
 az extension add --name azure-devops
 
 # Deploy Azure Analysis Server and ADF
-# Added 10/16/2018
+# Added 10/16/2018, updated 4/8/2019 to use dynamic population of json files.
 # Start deployment
 (
         set -x
@@ -227,13 +302,13 @@ fi
 echo "This Completes Part I, the physical resources deployment, Part II will now begin."
 
 # Populate Database Objects
-# Added 10/23/18, KGorman
+# Added data load and logic for sample data, 4/08/2019,  KGorman
 # Added proxy login to support scripts and added check log for post deploy. 10/25/2018
 # Updated to no longer just DDL, but added data to be loaded as part of DW and Staging data
 # DataWarehouse
-echo "Part II logs into the SQL Server and deploys the logical objects and support structures."
+echo "Part II logs into the SQL Server and deploys the logical objects, support structures and data if requested."
 
-if [ $data -eq 'Y' ]
+if [ $data -eq 1 ]
 then
   echo "Loading Example Schema and Data-  This will take about an hour to perform the data load."
   sqlcmd -U $adminlogin -S"${servername}.database.windows.net" -P "$password" -d master -Q "CREATE LOGIN HigherEDProxyUser WITH PASSWORD = '${proxypassword}'; "
@@ -254,17 +329,18 @@ else
   sqlcmd -U $adminlogin -S "${servername}.database.windows.net" -P "$password" -d HiEd_Staging -i "edu_ddl_staging.sql"
   echo "Object Build for both databases is complete.  No data was loaded, so zero rows are expected in counts of views"
 fi
+# Part II is now complete, onto Part III, logging
 
-
-echo "This is your Admin User,Password and Proxy Password:"  > edu_deploy.txt
-echo $adminlogin $password $proxypassword >> edu_deploy.txt
-echo "This is your Azure location zone:" $zone >> edu_deploy.txt
-echo "This is the SQL Server your created for the databases:" >> edu_deploy.txt
-echo $servername >> edu_deploy.txt
-echo "This is the subscription deployed to and the Firewall IP:" >> edu_deploy.txt
-echo $subscriptionID $myip >> edu_deploy.txt
+# Part III- Log all information from the deployment to the log file
+echo "This is your SQL Server, Admin User and Password:" > $logfile
+echo $servername $adminlogin $password >> $logfile
+echo "This is your ADF, Analysis Server and the Proxy Password for the AAS:"
+echo $adfname $aasname $proxypassword
+echo "This is your Azure location zone:" $zone >> $logfile
+echo "This is the subscription deployed to and the Firewall IP:" >> $logfile 
+echo $subscriptionID $myip >> $logfile
 echo "------------------------------------------------------------------------------------------------------------"
 # Check for data and push to output file
 sqlcmd -U $adminlogin -S "${servername}.database.windows.net" -P "$password" -d HiEd_Staging -i "ck_views.sql" > $logfile
 
-echo "Part II is now complete."
+echo "Part III is now complete."
